62af9a6818567c8d9d7a037d55c60c2091832fd3
Koiraladharana <164384178+Koiraladharana@users.noreply.github.com>
2024-10-23 12:25:07 +0545

    Update ghoste_prank.py

--- ghoste_prank.py
+++ ghoste_prank.py
@@ -1,1 +1,22 @@
+import pygame
+from time import sleep
 
+pygame.init()
+pygame.mixer.init()
+
+window = pygame.display.set_mode(0,0) ,pygame.FULLSCREEN
+
+pygame.mixer.music.load("")
+pygame.mixer.music.play("")
+sleep(5)
+
+pygame.mixer.music.load("")
+pygame.mixer.music.play("")
+sleep(1)
+
+image = pygame.image.load()
+window.blit(image ,(0,0))
+
+pygame.display.update()
+
+sleep(5)
